ae988854804c7cc0961f01b507bc12a235b0189d
Ethan <yun.k@naverz-corp.com>
2023-03-18 02:17:38 +0900

    Update

--- src/connector.ts
+++ src/connector.ts
@@ -4,7 +4,7 @@ import { isMobile, openLink } from 'src/utils';
 
 const dappMetadata = {
 	manifestUrl:
-		'https://gist.githubusercontent.com/siandreev/75f1a2ccf2f3b4e2771f6089aeb06d7f/raw/d4986344010ec7a2d1cc8a2a9baa57de37aaccb8/gistfile1.txt',
+		'https://github.com/niceoasi/PODO-dApp/blob/main/test.txt',
 };
 
 export const connector = new TonConnect(dappMetadata);
